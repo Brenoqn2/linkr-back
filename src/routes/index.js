60dbace7eac161e85722f406197dc8f71b741e47
Brenoqn2 <brenoqn2@gmail.com>
2022-06-14 19:24:18 -0300

    fix: formatting

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,15 +5,6 @@ import { ValidateUserToken } from '../middlewares/Authentication.js';
 
 const router = Router();
 
-// aqui não podemos usar o ValidateUserToken ( pq o usuário precisa registrar e logar )
 router.use(AuthenticationRouter);
-
-// a partir daqui já temos um middleware que vai validar se o token é válido
-// podemos usar esse middleware para todas as rotas que precisam de autenticação
-// como por exemplo:
-
-// router.use(PostsRouter, ValidateUserToken);
-// router.use(CommentsRouter, ValidateUserToken);
-// etc
 
 export default router;
