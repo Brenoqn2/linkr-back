c1fb4034da70a970f2a036c5884ac411ba0feb04
i-Lucas <novo.contato.lucas@gmail.com>
2022-06-16 19:05:37 -0300

    change user picture

--- src/routes/UsersRouter.js
+++ src/routes/UsersRouter.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import { getUserData } from '../controllers/UsersController.js';
+import { ValidateUserToken } from '../middlewares/Authentication.js';
+import { ChangeUserAvatarController } from '../controllers/UsersController.js';
 
 const UserRouter = Router();
 
 UserRouter.get('/user', getUserData);
+UserRouter.put('/change/avatar', ValidateUserToken, ChangeUserAvatarController);
 
 export default UserRouter;
