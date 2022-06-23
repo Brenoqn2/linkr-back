73dd27fd97547f7cfc4e7d4000cfaaaf05c2a8df
Brenoqn2 <brenoqn2@gmail.com>
2022-06-23 17:35:55 -0300

    feat: routes for infiniteScroll in UsersPage

--- src/routes/UsersRouter.js
+++ src/routes/UsersRouter.js
@@ -5,6 +5,7 @@ import {
   getUserPosts,
   getUsers,
   ChangeUserAvatarController,
+  checkMorePosts,
 } from '../controllers/UsersController.js';
 import { ValidateUserToken } from '../middlewares/Authentication.js';
 
@@ -14,5 +15,6 @@ UserRouter.get('/user', getUserData);
 UserRouter.get('/users', getUsers);
 UserRouter.get('/users/:id', getUserPosts);
 UserRouter.put('/change/avatar', ValidateUserToken, ChangeUserAvatarController);
+UserRouter.get('/users/checkPosts/:id', checkMorePosts);
 
 export default UserRouter;
